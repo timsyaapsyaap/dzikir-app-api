4deac29ae89862351fdbecf7cb57c2753fc82cc5
Fahmi Alfareza <fahmialfareza@icloud.com>
2022-07-22 12:50:04 +0700

    Refactoring code and add wg

--- repository/const.go
+++ repository/const.go
@@ -1,7 +1,13 @@
 package repository
 
+import "fmt"
+
 const (
 	findCityAddress = "/v1/sholat/kota/cari/"
 	cityDetails     = "/v1/sholat/kota/id/"
 	allCities       = "/v1/sholat/kota/semua"
 )
+
+func schedule(cityId int, year int, month int, date int) string {
+	return fmt.Sprintf("/v1/sholat/jadwal/%v/%v/%v/%v", cityId, year, month, date)
+}
