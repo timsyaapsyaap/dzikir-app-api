aa4922d229b36363ed3c4ae441becd6d7b8cee8b
Fahmi Alfareza <fahmialfareza@icloud.com>
2022-07-25 12:04:06 +0700

    Fix bugs verses by chapter

--- repository/const.go
+++ repository/const.go
@@ -14,6 +14,10 @@ func schedule(cityId int, year int, month int, date int) string {
 	return fmt.Sprintf("/v1/sholat/jadwal/%v/%v/%v/%v", cityId, year, month, date)
 }
 
-func versesByChapter(chapter int) string {
-	return fmt.Sprintf("/api/v4/verses/by_chapter/%v?language=id&fields=text_uthmani,text_indopak&translations=33&audio=7&word_fields=id", chapter)
+func getChapter(id int) string {
+	return fmt.Sprintf("/api/v4/chapters/%v?language=id", id)
+}
+
+func versesByChapter(chapter int, perPage int) string {
+	return fmt.Sprintf("/api/v4/verses/by_chapter/%v?language=id&fields=text_uthmani,text_indopak&translations=33&audio=7&word_fields=id&per_page=%v", chapter, perPage)
 }
