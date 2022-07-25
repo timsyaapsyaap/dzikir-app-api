09acf5d3611093dbb2e7d75ca42ca613b55bc3d6
Fahmi Alfareza <fahmialfareza@icloud.com>
2022-07-25 11:30:25 +0700

    Add get all chapters and verses by chapter (#3)

---
+++ controller/quran.go
@@ -0,0 +1,54 @@
+package controller
+
+import (
+	"strconv"
+
+	"github.com/fahmialfareza/dzikir-app-api/helper"
+	"github.com/fahmialfareza/dzikir-app-api/service"
+	"github.com/gin-gonic/gin"
+)
+
+type QuranController interface {
+	AllChapters(context *gin.Context)
+	VersesByChapter(context *gin.Context)
+}
+
+type quranController struct {
+	quranService service.QuranService
+}
+
+func NewQuranController(quranService service.QuranService) QuranController {
+	return &quranController{
+		quranService: quranService,
+	}
+}
+
+func (controller *quranController) AllChapters(context *gin.Context) {
+	data, err := controller.quranService.AllChapters()
+	if err != nil {
+		helper.BuildErrorResponse("Internal Server Error", err.Error(), nil)
+		return
+	}
+
+	res := helper.BuildResponse(true, "OK", data)
+	context.JSON(200, res)
+}
+
+func (controller *quranController) VersesByChapter(context *gin.Context) {
+	chapter := context.Param("chapter")
+
+	chapterInt, err := strconv.Atoi(chapter)
+	if err != nil {
+		helper.BuildErrorResponse("Internal Server Error", err.Error(), nil)
+		return
+	}
+
+	data, err := controller.quranService.VersesByChapter(chapterInt)
+	if err != nil {
+		helper.BuildErrorResponse("Internal Server Error", err.Error(), nil)
+		return
+	}
+
+	res := helper.BuildResponse(true, "OK", data)
+	context.JSON(200, res)
+}
--- repository/const.go
+++ repository/const.go
@@ -6,8 +6,14 @@ const (
 	findCityAddress = "/v1/sholat/kota/cari/"
 	cityDetails     = "/v1/sholat/kota/id/"
 	allCities       = "/v1/sholat/kota/semua"
+
+	allChapters = "/api/v4/chapters?language=id"
 )
 
 func schedule(cityId int, year int, month int, date int) string {
 	return fmt.Sprintf("/v1/sholat/jadwal/%v/%v/%v/%v", cityId, year, month, date)
+}
+
+func versesByChapter(chapter int) string {
+	return fmt.Sprintf("/api/v4/verses/by_chapter/%v?language=id&fields=text_uthmani,text_indopak&translations=33&audio=7&word_fields=id", chapter)
 }
